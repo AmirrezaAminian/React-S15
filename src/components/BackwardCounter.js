d58ebe523b0d0d242f000070a6f1986ffdc546ce
Amirreza Aminian <aminian.amirreza@gmail.com>
2023-05-25 15:04:22 +0430

    reviewig this section

--- src/components/BackwardCounter.js
+++ src/components/BackwardCounter.js
@@ -1,10 +1,18 @@
-import { useState, useEffect, useContext } from 'react';
-import useCounter from '../hooks/use-counter';
+import { useState, useEffect } from 'react';
 
 import Card from './Card';
 
 const BackwardCounter = () => {
-  const counter = useCounter(false);
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      // setCounter((prevCounter) => prevCounter - 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return <Card>{counter}</Card>;
 };
 
