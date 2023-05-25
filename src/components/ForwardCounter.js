cb75f960db093c1815bbd4ba5f3c424cac6081f0
Amirreza Aminian <aminian.amirreza@gmail.com>
2023-05-25 15:36:22 +0430

    Using Custom Hooks

--- src/components/ForwardCounter.js
+++ src/components/ForwardCounter.js
@@ -1,17 +1,13 @@
 import { useState, useEffect } from 'react';
 
 import Card from './Card';
+import useCounter from '../hooks/use-counter';
 
 const ForwardCounter = () => {
-  const [counter, setCounter] = useState(0);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      // setCounter((prevCounter) => prevCounter + 1);
-    }, 1000);
+ const counter =  useCounter() ;
 
-    return () => clearInterval(interval);
-  }, []);
+   
 
   return <Card>{counter}</Card>;
 };
---
+++ src/hooks/use-counter.js
@@ -0,0 +1,18 @@
+import { useEffect , useState } from "react";
+
+const useCounter = () => {
+
+     const [counter, setCounter] = useState(0);
+
+     useEffect(() => {
+       const interval = setInterval(() => {
+         setCounter((prevCounter) => prevCounter + 1);
+       }, 1000);
+
+       return () => clearInterval(interval);
+     }, []);
+
+     return counter ;
+}
+
+export default useCounter ;
